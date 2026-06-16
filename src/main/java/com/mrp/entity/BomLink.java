20b0e29122148a8f9d113bdab023e79633dd8d62
Prime-abhi <abhijeetkadam295@gmail.com>
2026-06-17 00:40:58 +0530

    Updated BomLink,BomLinkRepository

--- src/main/java/com/mrp/entity/BomLink.java
+++ src/main/java/com/mrp/entity/BomLink.java
@@ -1,5 +1,38 @@
 package com.mrp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bom_link")
 public class BomLink {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_item_id", nullable = false)
+    private Item parentItem;
+
+    @ManyToOne
+    @JoinColumn(name = "child_item_id", nullable = false)
+    private Item childItem;
+
+    @Column(nullable = false)
+    private Double quantityRequired;
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Item getParentItem() { return parentItem; }
+    public void setParentItem(Item parentItem) { this.parentItem = parentItem; }
+
+    public Item getChildItem() { return childItem; }
+    public void setChildItem(Item childItem) { this.childItem = childItem; }
+
+    public Double getQuantityRequired() { return quantityRequired; }
+    public void setQuantityRequired(Double quantityRequired) { 
+        this.quantityRequired = quantityRequired; 
+    }
 }
