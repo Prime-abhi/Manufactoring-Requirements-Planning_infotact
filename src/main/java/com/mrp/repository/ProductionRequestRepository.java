1b7a26bbe0a84a4a35e23003373f6da141330824
Ganesh patel <patelganesh8085494966@gmail.com>
2026-06-26 16:56:30 +0530

    updated backend files

--- src/main/java/com/mrp/repository/ProductionRequestRepository.java
+++ src/main/java/com/mrp/repository/ProductionRequestRepository.java
@@ -1,10 +1,1 @@
-package com.mrp.repository;
-
-import com.mrp.entity.ProductionRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductionRequestRepository extends JpaRepository<ProductionRequest, Long> {
-    List<ProductionRequest> findByItemId(Long itemId);
-    List<ProductionRequest> findByStatus(ProductionRequest.Status status);
-}
+ 
--- src/main/java/com/mrp/repository/UserRepository.java
+++ src/main/java/com/mrp/repository/UserRepository.java
@@ -1,10 +1,1 @@
-package com.mrp.repository;
-
-import com.mrp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    boolean existsByUsername(String username);
-}
+ 
